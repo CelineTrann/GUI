632178bc24a26b893481de870b4a0effd460745e
are-yaks-real <31707344+are-yaks-real@users.noreply.github.com>
2018-01-18 15:09:13 -0500

    Update LinearVolume.java

--- LinearVolume.java
+++ LinearVolume.java
@@ -6,131 +6,186 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created by 84674tra on 17/01/2018.
+ * Created by 84674tra on 18/01/2018.
  */
-public class LinearVolume extends JFrame {
+public class AreaFunction extends JFrame {
 
-    //initiates restrictions for calculations
-    private double m;
-    private double b;
-    private double d1;
-    private double d2;
-    private double gx;
-    private double slice;
-
-    //initiate volume variable
-    private double volume;
+    private double area;
+    private double height;
+    private double width;
+    private double rect;
 
     int Width = 300;
-    int Length = 150;
+    int Length = 300;
 
-    public LinearVolume (){
-        super("Linear Volume");
+    public AreaFunction (){
+        super("Area Function");
 
         setSize(Width, Length);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        // Set up heading for jframe
-        JLabel heading = new JLabel("Calculating Volume Under a Liner Function");
-        heading.setFont( new Font("Arial", Font.BOLD, 12));
+    }//close AreaFunction method
 
+    //-----------------------------------------------------------------------------------------------
+    public void Area (String type){
+
+        String header = null;
+        String function = null;
+
+        // Set up heading for jframe
+        switch (type){
+            case "Linear":
+                header = "Calculating Area Under a Liner Function";
+                function = "For the function: y = mx + b";
+                break;
+
+            case "Quadratic":
+                header = "Calculating Area Under a Quadratic Function";
+                function = "For the function: y = a (x - b)^2 + c";
+                break;
+
+            case "Sine":
+                header = "Calculating Area Under a Sine Function";
+                function = "For the function: y = a(sin[k (x - d)]) + c";
+                break;
+
+            case "Cosine":
+                header = "Calculating Area Under a Cosine Function";
+                function = "For the function: y = a(cos[k (x - d)]) + c";
+                break;
+        }
+        JLabel heading = new JLabel(header);
+        heading.setFont( new Font("Arial", Font.BOLD, 12));
+        
         // Create prompt for user
-        JLabel choose = new JLabel("For the function: f(x) = mx + b");
+        JLabel choose = new JLabel("For the function: " + function);
 
         //initiate instructions
-        JLabel mlabel, blabel, lowerD, upperD, vRestrict, iterations;
+        JLabel mlabel, alabel, blabel, clabel, dlabel, klabel, lowerD, upperD, vRestrict, iterations;
         mlabel = new JLabel("m value:");
+        alabel = new JLabel("a value:");
         blabel = new JLabel("b value:");
+        clabel = new JLabel("c value:");
+        dlabel = new JLabel("d value:");
+        klabel = new JLabel("k value:");
+        
         lowerD = new JLabel("lower domain:");
         upperD = new JLabel("upper domain:");
         vRestrict = new JLabel("vertical restriction:");
         iterations = new JLabel("Number of iterations:");
 
         //initiate text field
-        JTextField slope, vShift, D1, D2, Gx, iter;
-        slope = new JTextField();
-        vShift = new JTextField();
-        D1 = new JTextField();
-        D2 = new JTextField();
-        Gx = new JTextField();
-        iter = new JTextField();
+        JTextField slope, aValue, bValue, cValue, dValue, kValue, D1, D2, Gx, iter;
+        slope = new JTextField(12);
+        aValue = new JTextField(12);
+        bValue = new JTextField(12);
+        cValue = new JTextField(12);
+        dValue = new JTextField(12);
+        kValue = new JTextField(12);
 
+        D1 = new JTextField(12);
+        D2 = new JTextField(12);
+        Gx = new JTextField(12);
+        iter = new JTextField(12);
+
+        //create button and label that will show volume calculated
         JButton enter = new JButton("Enter");
-        JTextField VolumeDisplay = new JTextField();
-
+        JLabel AreaDisplay = new JLabel();
+        
         setLayout(new FlowLayout());
 
         // Add components to the JFrame
         add(heading);
         add(choose);
+        
+        switch (type){
+            case "Linear":
+                add(mlabel);        add(slope);
+                add(blabel);        add(bValue);
+                break;
 
-        add(mlabel);        add(slope);
-        add(blabel);        add(vShift);
+            case "Quadratic":
+                add(alabel);        add(aValue);
+                add(blabel);        add(bValue);
+                add(clabel);        add(cValue);
+                break;
+
+            case "Sine":
+                add(alabel);        add(aValue);
+                add(klabel);        add(kValue);
+                add(clabel);        add(cValue);
+                add(dlabel);        add(dValue);
+                break;
+
+            case "Cosine":
+                add(alabel);        add(aValue);
+                add(klabel);        add(kValue);
+                add(clabel);        add(cValue);
+                add(dlabel);        add(dValue);
+                break;
+            
+        }//close switch statement
+        
         add(lowerD);        add(D1);
         add(upperD);        add(D2);
         add(vRestrict);     add(Gx);
         add(iterations);    add(iter);
 
         add(enter);
-        add(VolumeDisplay);
-
+        add(AreaDisplay);
 
         enter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                m = Double.parseDouble(mlabel.getText());
-                b = Double.parseDouble(blabel.getText());
+                //initiate variables
+                double m, a, b, c, d, k, d1, d2, gx, slice;
+
+                //change user input to
+                m = Double.parseDouble(slope.getText());
+                b = Double.parseDouble(bValue.getText());
                 d1 = Double.parseDouble(D1.getText());
                 d2 = Double.parseDouble(D2.getText());
                 gx = Double.parseDouble(Gx.getText());
                 slice = Double.parseDouble(iter.getText());
 
-                Volume(m, b, d1, d2, gx, slice);
+                calcAreaLinear(m, b, d1, d2, gx, slice);
 
-                VolumeDisplay.setText("Your volume is:" + String.valueOf(volume));
-
+                AreaDisplay.setText("The area under the curve is:" + String.valueOf(area));
             }
         });
     }
 
-    private double Volume (double m, double b, double d1, double d2, double gx, double slice){
+    private double calcAreaLinear (double m, double b, double d1, double d2, double gx, double slice){
 
-        //initiates variables used to calculated volume
-        double x;
-        double thickness;
-        double height;
-        double width;
+        //reset area
+        area = 0;
 
-        //initiate for loop to allow for an iterative method of finding the volume
-        //int i is to restrict the iterative calculation to the domain range
-        for (int i = 0; i < slice; i++ ){
+        //Find y-value at the beginning of the domain
+        //i.e. its length
+        height = m*d1 + b - gx;
 
-            //calculate the width of each iterative slice
-            //calculated by dividing the horizontal range by the number of iterations
-            thickness = (d2 - d1)/slice;
+        //find how wide each slice is
+        width = (d2 - d1) / slice;
 
-            //allow the current iteration to be accounted for in the volume calculation
-            //calculates what the x-value is at that point in the iteration
-            x = d2 - thickness*i;
+        //will do loop as long as number of area left is less than number of sections
+        for (int x = 0; x < slice; x ++ ){
 
-            //what the height of said iteration is at the x -value
-            //height of the "cylinder" and will become height of rectangular prism
-            //uses linear eqn and subtract the g(x) value
-            height = m * x + b - gx;
+            //calculate the area of the rectangle
+            //width is how big each slice is
+            rect = Math.abs(width*height);
 
-            //calculates the circumference of the cylinder
-            //this will be used as the width of the rectangular prism
-            width = 2*Math.PI*x;
+            //move point to next slice
+            d1 += width;
 
-            //calculate the volume of each iteration and adds it to the overall volume
-            //does so by calculating the volume of a rectangular prism
-            //which is created by "peeling" the outer of the "cylinder" off
-            //which is created when the linear function is rotated around the y-axis
-            volume += Math.abs(width*height*thickness);
-        }//closes for loop
+            //find new y value
+            //add area of rectangle to overall area
+            height = m*d1 + b - gx;
+            area += rect;
 
-        //return volume value
-        return volume;
-    }
-    //close Volume function
+        }//end for loop
+
+        return area;
+
+    }//close calLinearArea Method
+
 
 }//close class
