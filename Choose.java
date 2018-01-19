4cc87a59ad9d0127710e91cc60f339c238348f5b
are-yaks-real <31707344+are-yaks-real@users.noreply.github.com>
2018-01-18 23:09:39 -0500

    Update Choose.java

--- Choose.java
+++ Choose.java
@@ -10,44 +10,68 @@ import java.awt.event.ActionListener;
  */
 public class Choose extends JFrame{
 
-    final int F_WIDTH   =  300;
-    final int F_HEIGHT  =  150;
+    //initiate variable to set JFrame size
+    final int WIDTH   =  300;
+    final int HEIGHT  =  200;
 
+    //-----------------------------------------------------------------------------------------------
+    //create objects for LinearVolume class and AreaFunction class
     LinearVolume linearVolume = new LinearVolume();
     AreaFunction areaFunction = new AreaFunction();
 
+    //-----------------------------------------------------------------------------------------------
+    //initiate Choose JFrame
     public Choose(){
 
         super("Choose");
 
-        setSize(F_WIDTH,F_HEIGHT);
+        //set size and close operation of frame
+        setSize(WIDTH,HEIGHT);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
 
-        // Here is our heading
+        //-------------------------------------------------------------------
+        //set heading of Jframe
         JLabel heading = new JLabel("Calculating Area and Volume");
         heading.setFont( new Font("Arial", Font.BOLD, 14));
 
-        // Here is our prompt
+        //initiate prompt for JFrame
         JLabel choose = new JLabel("What do you want to calculate");
 
+        //-------------------------------------------------------------------
+        //create options for comboBox
+        //initiate comboBox
         String areaOption[] = {"Volume", "Linear Area", "Quadratic Area", "Sine Area", "Cosine Area"};
         JComboBox comboBox = new JComboBox(areaOption);
 
+        //initiate comboBox for degrees and radians
+        String angleOptions[] = {"N/A", "Radians    ", "Degrees"};
+        JComboBox angleBox = new JComboBox(angleOptions);
+
+        //create choose button
         JButton show = new JButton("Choose!");
 
+        //-------------------------------------------------------------------
+        //set JFrame layout ot Flow Layout
         setLayout(new FlowLayout());
 
         // Add components to the JFrame
         add(heading);
         add(choose);
         add(comboBox);
+        add(angleBox);
         add(show);
 
-
+        //-------------------------------------------------------------------
+        //initiate actionListener when show button is clicked
         show.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+
+                //get what user selected in comboBox
                 String selected = (String) comboBox.getSelectedItem();
 
+                //show new Jframe depending on user option in comboBox
+                //dispose current JFrame
                 switch (selected){
                     case "Volume":
                         linearVolume.setVisible(true);
@@ -61,25 +85,40 @@ public class Choose extends JFrame{
                         break;
 
                     case "Quadratic Area":
+                        areaFunction.Area("Quadratic");
+                        areaFunction.setVisible(true);
+                        dispose();
                         break;
 
                     case "Sine Area":
+                        //get what user selected in angleBox
+                        String sinAngle = (String) angleBox.getSelectedItem();
+
+                        //call for areaFunction Jframe to appear
+                        areaFunction.setAngle(sinAngle);
+                        areaFunction.Area("Sine");
+                        areaFunction.setVisible(true);
+
+                        //get rid of current JFrame
+                        dispose();
                         break;
 
                     case "Cosine Area":
+                        //get what user selected in angleBox
+                        String cosAngle = (String) angleBox.getSelectedItem();
+
+                        //call for areaFunction Jframe to appear
+                        areaFunction.setAngle(cosAngle);
+                        areaFunction.Area("Cosine");
+                        areaFunction.setVisible(true);
+
+                        //get rid of current JFrame
+                        dispose();
                         break;
-                }
 
-               dispose();
-            }
-        });
+                }//close switch statement
+            }//close actionPerformed method
+        });//close actionListener
 
-
-
-    }
-
-
-
-
-
-}
+    }//close choose JFrame
+}//close Choose class
